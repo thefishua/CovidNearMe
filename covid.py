f0eead30ffa843e9cb3d259c23aac3c6948f9a29
Liam <z5311885@ad.unsw.edu.au>
2021-08-26 20:44:58 +1000

    Started trying to use sql query instead of standard query

--- covid.py
+++ covid.py
@@ -8,6 +8,8 @@ from datetime import date, timedelta
 COVID_INFECTION_PERIOD = 15
 BASE = "https://data.nsw.gov.au/data"
 RESOURCE = '/api/3/action/datastore_search?resource_id=21304414-1ff1-4243-a5d2-f52778048b29'
+RESOURCE_CODE = "21304414-1ff1-4243-a5d2-f52778048b29"
+RESOURCE_SQL = "/api/3/action/datastore_search_sql?sql="
 LGA_FILE = "lga_dict.json"
 LGA_NAME = "LGA_NAME19"
 LGA_CODE = "LGA_CODE19"
@@ -67,13 +69,33 @@ def casesActivePerLGA(lga_code: int) -> int:
         iter_past = iter_past + timedelta(days=1)
     return cases       
 
-        
+def casesActivePerLGA_SQL(lga_code: int) -> int:
+    today = date.today()
+    past = today - timedelta(days=COVID_INFECTION_PERIOD)
+    location = BASE + RESOURCE_SQL
+    s_context = ssl.SSLContext()
+    cases = 0
+    query = f"SELECT * from \"{RESOURCE_CODE}\" WHERE lga_code19 = \"jones\" "
+    query += "AND notification_date BETWEEN \"{past}\" AND \"{today}\""
+    # print(yelp)
+    ulocation = location + query
+    print(ulocation)
+    obj = request.urlopen(ulocation, context = s_context)
+    data = json.load(obj)
+    if data['success']:
+        # print(data['result']['total'])
+        cases += int(data["result"]["total"])
+            
+        # print(data)
+    iter_past = iter_past + timedelta(days=1)
+    return cases        
     
 
 
 if __name__ == "__main__":
-    f = open("lga.json")
-    lga = json.load(f)
-    for area in lga:
-        count = casesActivePerLGA(area[LGA_CODE])
-        print(f"Active cases in {area[LGA_NAME]}: {count}")
+    # f = open("lga.json")
+    # lga = json.load(f)
+    # for area in lga:
+    #     count = casesActivePerLGA(area[LGA_CODE])
+    #     print(f"Active cases in {area[LGA_NAME]}: {count}")
+    casesActivePerLGA_SQL(18250)
