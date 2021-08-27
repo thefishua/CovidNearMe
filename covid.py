a0742a67024b4f806268024fcaf7cfcd6ebec6d3
Liam <z5311885@ad.unsw.edu.au>
2021-08-27 12:53:06 +1000

    Implemented SQL version of casesActivePerLGA, significant improvement in run time

--- covid.py
+++ covid.py
@@ -8,7 +8,7 @@ from datetime import date, timedelta
 COVID_INFECTION_PERIOD = 15
 BASE = "https://data.nsw.gov.au/data"
 RESOURCE = '/api/3/action/datastore_search?resource_id=21304414-1ff1-4243-a5d2-f52778048b29'
-RESOURCE_CODE = "21304414-1ff1-4243-a5d2-f52778048b29"
+RESOURCE_CODE = '2776dbb8-f807-4fb2-b1ed-184a6fc2c8aa'
 RESOURCE_SQL = "/api/3/action/datastore_search_sql?sql="
 LGA_FILE = "lga_dict.json"
 LGA_NAME = "LGA_NAME19"
@@ -39,7 +39,7 @@ def casesActiveNSW():
         past = past + timedelta(days=1)
 
     return cases
-
+       
 # This function calculates the amount of active covid cases in NSW
 # Specific to an local government area
 # List of LGA's and their codes can be found in lga.json
@@ -48,54 +48,28 @@ def casesActiveNSW():
 def casesActivePerLGA(lga_code: int) -> int:
     today = date.today()
     past = today - timedelta(days=COVID_INFECTION_PERIOD)
-    iter_past = past
-    location = BASE + RESOURCE
-    s_context = ssl.SSLContext()
-    cases = 0
-    while iter_past != today:
-        query = "{\"notification_date\":\"" + str(iter_past)
-        query += "\",\"lga_code19\":\"" + str(lga_code) + "\"}"
-        yelp = f"&filters={query}"
-        # print(yelp)
-        ulocation = location + yelp
-        # print(ulocation)
-        obj = request.urlopen(ulocation, context = s_context)
-        data = json.load(obj)
-        if data['success']:
-            # print(data['result']['total'])
-            cases += int(data["result"]["total"])
-                
-            # print(data)
-        iter_past = iter_past + timedelta(days=1)
-    return cases       
-
-def casesActivePerLGA_SQL(lga_code: int) -> int:
-    today = date.today()
-    past = today - timedelta(days=COVID_INFECTION_PERIOD)
     location = BASE + RESOURCE_SQL
     s_context = ssl.SSLContext()
     cases = 0
-    query = f"SELECT * from \"{RESOURCE_CODE}\" WHERE lga_code19 = \"jones\" "
-    query += "AND notification_date BETWEEN \"{past}\" AND \"{today}\""
+    query = f"SELECT+*+from+\"{RESOURCE_CODE}\"+WHERE+lga_code19+LIKE+\'{lga_code}\'+"
+    query += f"AND+notification_date+BETWEEN+\'{past}\'+AND+\'{today}\'"
     # print(yelp)
     ulocation = location + query
-    print(ulocation)
+    # print(ulocation)
     obj = request.urlopen(ulocation, context = s_context)
     data = json.load(obj)
     if data['success']:
         # print(data['result']['total'])
-        cases += int(data["result"]["total"])
+        cases += len(data["result"]["records"])
             
         # print(data)
-    iter_past = iter_past + timedelta(days=1)
     return cases        
     
 
 
 if __name__ == "__main__":
-    # f = open("lga.json")
-    # lga = json.load(f)
-    # for area in lga:
-    #     count = casesActivePerLGA(area[LGA_CODE])
-    #     print(f"Active cases in {area[LGA_NAME]}: {count}")
-    casesActivePerLGA_SQL(18250)
+    f = open("lga.json")
+    lga = json.load(f)
+    for area in lga:
+        count = casesActivePerLGA(area[LGA_CODE])
+        print(f"Active cases in {area[LGA_NAME]}: {count}")
