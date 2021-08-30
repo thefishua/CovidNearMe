31cb8896a1126a18a854555e94b96d6d1912199b
Joshua Fish <josh.fishua@gmail.com>
2021-08-30 16:46:43 +1000

    SideBar added pages, components, cleaned app.js, added files for different screens

---
+++ src/frontend/covid-near-me/src/pages/Vaccinations.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Vaccinations() {
+    return (
+        <div className='vaccination'>
+            Vaccination
+        </div>
+    )
+}
+
+export default Vaccinations
