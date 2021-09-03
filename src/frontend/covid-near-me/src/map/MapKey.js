eed4729352b16cbc6aa78e3869a792a6344d18a3
Joshua Fish <josh.fishua@gmail.com>
2021-09-03 15:31:11 +1000

    MapKey added to active cases map

--- src/frontend/covid-near-me/src/map/MapKey.js
+++ src/frontend/covid-near-me/src/map/MapKey.js
@@ -1,39 +1,24 @@
 import React from 'react'
+import {MapKeyData} from './MapKeyData'
 import "./MapKey.css"
 function MapKey() {
     return (
         <div className="box">
-            <img 
-                className = 'marker'
-                src={"/mapbox-marker-icon-blue.svg"} 
-                alt="Marker"> 
-                
-            </img>
-            <img 
-                className = 'marker'
-                src={"/mapbox-marker-icon-red.svg"} 
-                alt="Marker"> 
-                
-            </img>
-
-            <img 
-                className = 'marker'
-                src={"/mapbox-marker-icon-orange.svg"} 
-                alt="Marker"> 
-                
-            </img>
-
-            <img 
-                className = 'marker'
-                src={"/mapbox-marker-icon-yellow.svg"} 
-                alt="Marker"> 
-            </img>
-
-            <img 
-                className = 'marker'
-                src={"/mapbox-marker-icon-green.svg"} 
-                alt="Marker"> 
-            </img>
+            <div className="box-title"> Key </div>
+            <ul className="box-items">       
+                {/* Going through all the map keys in the MapKeyData file and displaying them
+                    With the specified image and the specified text*/}
+                {MapKeyData.map((item, index) => {
+                    return (
+                        <li key={index} className={item.cName}>
+                            {item.image}
+                            <span>
+                                {item.text}
+                            </span>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
