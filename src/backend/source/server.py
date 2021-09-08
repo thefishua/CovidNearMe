3f8067291ee7d7ddb42d5495fea3b1d3fa55ccd7
Liam <z5311885@ad.unsw.edu.au>
2021-09-08 17:31:54 +1000

    test

--- src/backend/source/server.py
+++ src/backend/source/server.py
@@ -25,7 +25,7 @@ def defaultHandler(err):
     response.content_type = 'application/json'
     return response
 
-APP = Flask(__name__, static_url_path='', static_folder='build/')
+APP = Flask(__name__, static_url_path='', static_folder='../../frontend/covid-near-me/build')
 APP.config['TRAP_HTTP_EXCEPTIONS'] = True
 APP.register_error_handler(Exception, defaultHandler)
 CORS(APP)
