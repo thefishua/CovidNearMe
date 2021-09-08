fcff3e46f8ce824eca322ed19fa7b9f610cc1f32
Liam <z5311885@ad.unsw.edu.au>
2021-09-08 14:36:55 +1000

    Added MapKey

--- src/frontend/covid-near-me/src/map/MapKey.js
+++ src/frontend/covid-near-me/src/map/MapKey.js
@@ -1,26 +1,42 @@
-import React from 'react'
+import React from 'react';
 // import {MapKeyData} from './ActiveCaseMapKeyData'
-import "./MapKey.css"
-function MapKey(props) {
+import Collapsible from 'react-collapsible';
+import "./MapKey.css";
+function MapKey(props) { 
+    
     return (
-        <div className="box">
-            <div className="box-title"> Key </div>
-            <ul className="box-items">       
-                {/* Going through all the map keys in the MapKeyData file and displaying them
-                    With the specified image and the specified text*/}
-                {props.MapKeyData.map((item, index) => {
-                    return (
-                        <li key={index} className={item.cName}>
-                            {item.image}
-                            <span>
-                                {item.text}
-                            </span>
-                        </li>
-                    )
-                })}
-            </ul>
+    <div className="box">
+      <Collapsible 
+        trigger="Key" 
+        className='box-title'
+        triggerClassName="closedTitle"
+        triggerOpenedClassName="openTitle"    
+        openedClassName="box-title-open"
+
+    >
+        <div className="box-items">
+          {key(props)}
         </div>
+      </Collapsible>
+    </div>
+
+    );
+}
+
+function key(props) {
+    return (
+        props.MapKeyData.map((item, index) => {
+            return (
+                <li key={index} className={item.cName}>
+                    {item.image}
+                    <span>
+                        {item.text}
+                    </span>
+                </li>
+            )
+        })
     )
 }
+
 
 export default MapKey
